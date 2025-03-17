961e8337344038429960d8df6254d65770c59e44
JonArbell <138678861+JonArbell@users.noreply.github.com>
2025-03-17 09:50:42 +0800

    try

--- src/main/java/com/my_portfolio/backend/Controller/PingController.java
+++ src/main/java/com/my_portfolio/backend/Controller/PingController.java
@@ -1,6 +1,5 @@
 package com.my_portfolio.backend.Controller;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
